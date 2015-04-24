84d6eaf893d0798d8438a549b92c9711ffa49a71
spravo13 <spencer@techandcode.com>
2015-04-23 17:03:25 -0700

    Make else for all but remainder = 1
    Changes all except for "=0".

--- cipher.py
+++ cipher.py
@@ -6,19 +6,15 @@ code = 28
 letters = array('u', ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"])
 remainder = code % 26
 first_letter = 28 - remainder
-print(first_letter)
 if remainder == 1:
-		letters = letters
-elif remainder == 2:
-		#letters = array('u', ["main_array['first_letter']", "main_array"])
-		letters.insert(0,letters[first_letter-1])
-		del letters[first_letter]
-#elif remainder == 3:
-		
-
-
-#elif remainder == 24:
-
+	letters = letters
+else:
+	numbers = list(range(first_letter,27))
+	start = 0
+	for number in numbers:
+		letters.insert(start,letters[number-1])
+		del letters[number]
+		start = start + 1
 
 for letter in letters:
-  print(letter)
+	print(letter)
