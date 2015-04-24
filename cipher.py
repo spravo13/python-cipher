87adb47f47ebce295c0dc817d88b34ccb8dd9b35
spravo13 <spencer@techandcode.com>
2015-04-24 15:58:52 -0700

    Change single quotes to double quotes.
    Makes uniform with array values.

--- cipher.py
+++ cipher.py
@@ -2,7 +2,7 @@
 
 from array import array
 
-CODE = int(input('Enter your number: '))
+CODE = int(input("Enter your number: "))
 LETTERS = array('u', ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", \
 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
 REMAINDER = CODE % 26
