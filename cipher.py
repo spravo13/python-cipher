e71860578446e6642cc182a74a44b31424483eb6
spravo13 <spencer@techandcode.com>
2015-04-23 22:04:48 -0700

    Add comment, del system specifics, minimize import

--- cipher.py
+++ cipher.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3.2
-from array import *
+"""Check remainder from alphabet and rearrange letters based on outcome"""
+
+from array import array
 
 CODE = int(input('Enter your number: '))
-LETTERS = array('u', ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+LETTERS = array('u', ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", \
+"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
 REMAINDER = CODE % 26
 FIRST_LETTER = 27 - REMAINDER
 if REMAINDER == 0:
