b70557ba9966c725919d5ad4d4ae600468ceef0c
spravo13 <spencer@techandcode.com>
2015-04-23 17:37:50 -0700

    Shift switching down one letter.
    ie:		0 = no change
    		1 = 1 up
    		etc.

--- cipher.py
+++ cipher.py
@@ -4,8 +4,8 @@ from array import *
 code = int(input('Enter your number: '))
 letters = array('u', ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"])
 remainder = code % 26
-first_letter = 28 - remainder
-if remainder == 1:
+first_letter = 27 - remainder
+if remainder == 0:
     letters = letters
 else:
     numbers = list(range(first_letter,27))
