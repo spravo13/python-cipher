a172514d971dbeea2a4db5f637384ddd9a9559f8
Spencer Bravo <spencer@codeshrub.com>
2015-08-02 13:26:29 -0700

    Add support for spaces and special characters

--- cipher.py
+++ cipher.py
@@ -6,6 +6,8 @@ ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", \
 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", \
 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+SPECIAL_CHARACTERS = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", \
+"_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"]
 REMAINDER = PIN % 26
 FIRST_LETTER = 27 - REMAINDER
 
@@ -25,19 +27,35 @@ else:
 MESSAGE = list(input("Enter your message: ").lower())
 
 #Encode message to numbers
-if MESSAGE != "":
-    #Create empty list for the string encoded to numbers
-    NUMBER_ENCODED = []
-    for LETTER in MESSAGE:
-        #Add each number that comes from the letter to the list
-        NUMBER_ENCODED.append(ALPHABET.index(LETTER))
+
+#Create empty list for the string encoded to numbers
+NUMBER_ENCODED = []
+for LETTER in MESSAGE:
+    #Add each number that comes from the letter to the list
+    if LETTER != ' ':
+        SPECIAL_CHECK = LETTER in SPECIAL_CHARACTERS
+        if SPECIAL_CHECK == False:
+            NUMBER_ENCODED.append(ALPHABET.index(LETTER))
+        else:
+            NUMBER_ENCODED.append(LETTER)
+    else:
+        NUMBER_ENCODED.append(" ")
 
 #Encode message to scrambled words
+
 #Create empty list for the encoded numbers to go to the encode message
 MESSAGE_ENCODED = []
 for NUMBER in NUMBER_ENCODED:
     #Add each letter that corrisponds to the number in the LETTERS list
-    MESSAGE_ENCODED.append(LETTERS[NUMBER])
+    if NUMBER != ' ':
+        SPECIAL_CHECK = NUMBER in SPECIAL_CHARACTERS
+        if SPECIAL_CHECK == False:
+            MESSAGE_ENCODED.append(LETTERS[NUMBER])
+        else:
+            MESSAGE_ENCODED.append(NUMBER)
+    else:
+        MESSAGE_ENCODED.append(" ")
+
 
 ENCODED_STRING = ''.join(str(l) for l in MESSAGE_ENCODED)
 
